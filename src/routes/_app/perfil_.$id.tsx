ee3cec2144d89041aeb91ca8e1acc3b8f8953f3e
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2026-05-13 19:50:35 +0000

    fix(perfil): romper anidamiento de rutas perfil/$id para que linkee al perfil del owner correcto

--- src/routes/_app/perfil_.$id.tsx
+++ src/routes/_app/perfil_.$id.tsx
@@ -2,10 +2,10 @@ import { createFileRoute, useParams } from '@tanstack/react-router'
 import { PerfilPage } from '@/features/perfil/components/PerfilPage'
 
 function PerfilByIdRoute() {
-  const { id } = useParams({ from: '/_app/perfil/$id' })
+  const { id } = useParams({ from: '/_app/perfil_/$id' })
   return <PerfilPage profileId={id} />
 }
 
-export const Route = createFileRoute('/_app/perfil/$id')({
+export const Route = createFileRoute('/_app/perfil_/$id')({
   component: PerfilByIdRoute,
 })
