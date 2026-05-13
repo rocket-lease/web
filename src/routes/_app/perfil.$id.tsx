255f4275e8f1e4bdef50687f70787cc6d4e607e9
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2026-05-13 19:33:24 +0000

    feat(detalle): owner card linkea al perfil público del rentador (US-20 AC3)

--- src/routes/_app/perfil.$id.tsx
+++ src/routes/_app/perfil.$id.tsx
@@ -1,9 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from '@tanstack/react-router'
+import { PerfilPage } from '@/features/perfil/components/PerfilPage'
+
+function PerfilByIdRoute() {
+  const { id } = useParams({ from: '/_app/perfil/$id' })
+  return <PerfilPage profileId={id} />
+}
 
 export const Route = createFileRoute('/_app/perfil/$id')({
-  component: RouteComponent,
+  component: PerfilByIdRoute,
 })
-
-function RouteComponent() {
-  return <div>Hello "/_app/perfil/$id"!</div>
-}
